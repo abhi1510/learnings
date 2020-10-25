cc5823a3aca7935304e0deb18daf2d0f3eed6109
Abhinav Kumar <abhinav.k@3ktechnologies.com>
2020-10-25 21:30:14 +0530

    adding project: basics

---
+++ AngularTheCompleteGuide/project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test', 'This is a test recipe', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+    new Recipe('Test', 'This is a test recipe', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+    new Recipe('Test', 'This is a test recipe', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
