a18c500c3b3a5ead7e6a94ed074cfaedd3e4ecd1
Abhinav Kumar <abhinav.k@3ktechnologies.com>
2020-10-26 12:10:00 +0530

    adding project: recipe items

--- AngularTheCompleteGuide/project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ AngularTheCompleteGuide/project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,9 +9,11 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Test 1', 'This is a test recipe', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
-    new Recipe('Test 2', 'This is a test recipe', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
-    new Recipe('Test 3', 'This is a test recipe', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+    new Recipe('Test 1', 'This is a test 1 recipe', 'https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+    new Recipe('Test 2', 'This is a test 2 recipe', 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1035&q=80'),
+    new Recipe('Test 3', 'This is a test 3 recipe', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+    new Recipe('Test 4', 'This is a test 4 recipe', 'https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80'),
+    new Recipe('Test 5', 'This is a test 5 recipe', 'https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80')
   ];
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
