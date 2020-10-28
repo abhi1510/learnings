90d7cbf9cd844243e00d077e44750a43c7cc3cc8
Abhinav Kumar <abhinav.k@3ktechnologies.com>
2020-10-28 18:38:02 +0530

    adding section 10, 11, 12 src and project: routing

--- AngularTheCompleteGuide/project/src/app/header/header.component.ts
+++ AngularTheCompleteGuide/project/src/app/header/header.component.ts
@@ -1,23 +1,8 @@
-import { Component, EventEmitter, Output, OnInit } from '@angular/core';
-import { features } from 'process';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  @Output() featureSelected = new EventEmitter<string>(); 
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSelect(feature: string) {
-    console.log(feature);
-    this.featureSelected.emit(feature);
-  }
-
-}
+export class HeaderComponent {}
--- AngularTheCompleteGuide/project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ AngularTheCompleteGuide/project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,16 +9,23 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
+  id: number;
+  recipe: Recipe;
 
-  @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute, 
+    private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    })
   }
 
-  onAddToShoppingList() {
-    this.recipeService.addRecipesToShoppingList(this.recipe.ingredients);
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
